32c5a20f38fea11c59e38bcb9e2a30b0b338a807
allexis096 <allexissoares@gmail.com>
2020-11-26 20:48:29 -0300

    :sparkles: feat: add AppRoutes

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Dashboard from '../pages/Dashboard';
+
+const App = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <App.Navigator
+    screenOptions={{
+      headerTitle: 'POKÉMON CHALLENGE',
+      headerTintColor: '#fff',
+      headerStyle: { backgroundColor: '#28262E' },
+      headerTitleAlign: 'center',
+      cardStyle: { backgroundColor: '#312E38' },
+    }}
+  >
+    <App.Screen name="Dashboard" component={Dashboard} />
+  </App.Navigator>
+);
+
+export default AppRoutes;
