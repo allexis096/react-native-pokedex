6be1d04acb7b51b008b64e8346e032dad7c76ccf
allexis096 <allexissoares@gmail.com>
2020-11-26 20:49:28 -0300

    :zap: chore: using react-navigation and routes on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import Dashboard from './pages/Dashboard';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import AppRoutes from './routes';
 
 const App: React.FC = () => {
-  return <Dashboard />;
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312E38" />
+      <AppRoutes />
+    </NavigationContainer>
+  );
 };
 
 export default App;
