145e0c89aad19b8e9fa6776f414b8ef5f6ee1a98
allexis096 <allexissoares@gmail.com>
2020-11-27 00:10:02 -0300

    :sparkles: feat: add Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import {
+  PokeCard,
+  PokeNumber,
+  Pokemon,
+  PokeNames,
+  PokeName,
+  PokeStrong,
+  PokeTypes,
+  PokeType,
+  PokeInfoType,
+} from './styles';
+
+interface CardProps {
+  poke_number: number;
+  poke_img: string;
+  poke_name: string;
+  poke_type: string;
+}
+
+const Card: React.FC<CardProps> = ({
+  poke_number,
+  poke_img,
+  poke_name,
+  poke_type,
+}) => {
+  return (
+    <PokeCard>
+      <PokeNumber>{`#${poke_number}`}</PokeNumber>
+
+      <Pokemon source={{ uri: poke_img }} />
+
+      <PokeNames>
+        <PokeName>Name: </PokeName>
+        <PokeStrong>{poke_name}</PokeStrong>
+      </PokeNames>
+
+      <PokeTypes>
+        <PokeType>Types: </PokeType>
+        <PokeInfoType>{poke_type}</PokeInfoType>
+      </PokeTypes>
+    </PokeCard>
+  );
+};
+
+export default Card;
---
+++ src/components/Card/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components/native';
+
+export const PokeCard = styled.TouchableOpacity`
+  position: relative;
+
+  background-color: #3e3b47;
+  width: 47%;
+  align-items: center;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+export const PokeNumber = styled.Text`
+  position: absolute;
+  left: 5px;
+
+  color: #666360;
+  font-family: 'RobotoSlab-Bold';
+`;
+
+export const Pokemon = styled.Image`
+  width: 50px;
+  height: 50px;
+  margin-top: 10px;
+`;
+
+export const PokeNames = styled.View`
+  flex-direction: row;
+  margin: 10px 0;
+`;
+
+export const PokeName = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const PokeStrong = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const PokeTypes = styled.View`
+  flex-direction: row;
+  margin-bottom: 10px;
+`;
+
+export const PokeType = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const PokeInfoType = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Regular';
+`;
