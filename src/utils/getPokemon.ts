6ebb24e251d9a407ba4f7dc5f8881546e87aff91
allexis096 <allexissoares@gmail.com>
2020-11-29 17:33:18 -0300

    :bulb: chore: created id on getPokemon function

--- src/utils/getPokemon.ts
+++ src/utils/getPokemon.ts
@@ -1,14 +1,22 @@
 import { PokemonProps } from '../pages/Dashboard';
+import api from '../services/api';
 
-export async function getPokemon(url: string): Promise<PokemonProps> {
-  const response = await fetch(url).then(res => res.json());
+export async function getPokemon(name: string): Promise<PokemonProps> {
+  const { data } = await api.get(`pokemon/${name}`);
+  const {
+    data: {
+      evolution_chain: { url },
+    },
+  } = await api.get(`pokemon-species/${name}`);
+  const id = url.substring(url.length - 2).replace('/', '');
 
   const pokemon: PokemonProps = {
-    typesPoke: response.types,
-    name: response.name,
-    image: response.sprites.front_default,
-    height: response.height,
-    weight: response.weight,
+    typesPoke: data.types,
+    name: data.name,
+    image: data.sprites.front_default,
+    height: data.height,
+    weight: data.weight,
+    id,
   };
 
   return pokemon;
