0bd9d9f8ebddcf9dc88a043e94e3f8e737c96f8d
allexis096 <allexissoares@gmail.com>
2020-11-29 14:04:24 -0300

    :zap: chore: PokemonStats connected to API

--- src/pages/PokemonStats/index.tsx
+++ src/pages/PokemonStats/index.tsx
@@ -1,23 +1,124 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FlatList } from 'react-native-gesture-handler';
+import ProgressBar from '../../components/ProgressBar';
+import api from '../../services/api';
 
-import { Text } from 'react-native';
-
-import { Container } from './styles';
+import {
+  Container,
+  PokeProfile,
+  PokeNumber,
+  Pokemon,
+  PokeStrong,
+  PokeSize,
+  Size,
+  SizeNumber,
+  SizeText,
+  Stats,
+} from './styles';
 
 interface RouteParams {
   route: {
     params: {
       name: string;
+      image: string;
+      number: number;
+      height: number;
+      weight: number;
     };
   };
 }
 
+interface StatsProps {
+  base_stat: number;
+  stat: {
+    name: 'hp' | 'attack' | 'defense' | 'speed';
+  };
+}
+
+interface StatsNumbersProps {
+  name: string;
+  base_stat: number;
+}
+
 const PokemonStats: React.FC<RouteParams> = ({ route }) => {
-  const { name } = route.params;
+  const { name, image, number, height, weight } = route.params;
+  const [statsNumbers, setStatsNumbers] = useState<StatsNumbersProps[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const statsNumber: StatsNumbersProps[] = [];
+      const {
+        data: { stats },
+      } = await api.get(`pokemon/${name}`);
+
+      await stats.find(
+        (stat: StatsProps) =>
+          stat.stat.name === 'hp' &&
+          statsNumber.push({
+            name: stat.stat.name.replace('hp', 'hp  '),
+            base_stat: stat.base_stat,
+          }),
+      );
+      await stats.find(
+        (stat: StatsProps) =>
+          stat.stat.name === 'attack' &&
+          statsNumber.push({
+            name: stat.stat.name.replace('attack', 'atk'),
+            base_stat: stat.base_stat,
+          }),
+      );
+      await stats.find(
+        (stat: StatsProps) =>
+          stat.stat.name === 'defense' &&
+          statsNumber.push({
+            name: stat.stat.name.replace('defense', 'def'),
+            base_stat: stat.base_stat,
+          }),
+      );
+      await stats.find(
+        (stat: StatsProps) =>
+          stat.stat.name === 'speed' &&
+          statsNumber.push({
+            name: stat.stat.name.replace('speed', 'spd'),
+            base_stat: stat.base_stat,
+          }),
+      );
+
+      setStatsNumbers(statsNumber);
+    })();
+  }, [name]);
 
   return (
-    <Container style={{ flex: 1 }}>
-      <Text>{name}</Text>
+    <Container>
+      <PokeProfile>
+        <PokeNumber>{`# ${number}`}</PokeNumber>
+
+        <Pokemon source={{ uri: image }} />
+
+        <PokeStrong>{name}</PokeStrong>
+
+        <PokeSize>
+          <Size>
+            <SizeNumber>{`${weight} KG`}</SizeNumber>
+            <SizeText>Weight</SizeText>
+          </Size>
+
+          <Size>
+            <SizeNumber>{`${height} M`}</SizeNumber>
+            <SizeText>Height</SizeText>
+          </Size>
+        </PokeSize>
+
+        <Stats>Stats</Stats>
+
+        <FlatList
+          data={statsNumbers}
+          keyExtractor={stats => stats.name}
+          renderItem={({ item: stats }) => (
+            <ProgressBar title={stats.name} percentage={stats.base_stat} />
+          )}
+        />
+      </PokeProfile>
     </Container>
   );
 };
--- src/pages/PokemonStats/styles.ts
+++ src/pages/PokemonStats/styles.ts
@@ -1,3 +1,65 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.SafeAreaView`
+  align-items: center;
+  margin-top: 10px;
+`;
+
+export const PokeProfile = styled.View`
+  position: relative;
+
+  background-color: #3e3b47;
+  width: 70%;
+  align-items: center;
+  margin-left: 8px;
+  margin-bottom: 8px;
+`;
+
+export const PokeNumber = styled.Text`
+  position: absolute;
+  left: 5px;
+
+  color: #666360;
+  font-family: 'RobotoSlab-Bold';
+`;
+
+export const Pokemon = styled.Image`
+  width: 120px;
+  height: 120px;
+  margin-top: 10px;
+`;
+
+export const PokeStrong = styled.Text`
+  color: #ff9000;
+  font-family: 'RobotoSlab-Regular';
+  text-transform: capitalize;
+`;
+
+export const PokeSize = styled.View`
+  flex-direction: row;
+  margin-bottom: 10px;
+`;
+
+export const Size = styled.View`
+  flex: 0.4;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px 0px;
+`;
+
+export const SizeNumber = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const SizeText = styled.Text`
+  color: #666360;
+  font-family: 'RobotoSlab-Regular';
+  margin-top: -5px;
+`;
+
+export const Stats = styled.Text`
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Regular';
+  margin-bottom: 10px;
+`;
